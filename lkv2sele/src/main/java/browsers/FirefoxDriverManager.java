10156a6acc54ea34befa74d135337bdfae389784
Lam Nguyen <nguyentunglam912@gmail.com>
2020-03-17 09:30:58 +0700

    Update framework

--- lkv2sele/src/main/java/browsers/FirefoxDriverManager.java
+++ lkv2sele/src/main/java/browsers/FirefoxDriverManager.java
@@ -2,14 +2,13 @@ package browsers;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import helper.FileReaderManager;
+import helper.DriverUtils;
 import managers.DriverManager;
 
 public class FirefoxDriverManager extends DriverManager{
 	@Override
 	public void createWebDriver() {
-		String driverPath =  FileReaderManager.getInstance().getConfigReader().getDriverPath();
-		System.setProperty("webdriver.gecko.driver", driverPath);	
+		System.setProperty("webdriver.gecko.driver", DriverUtils.driverPath + "geckodriver.exe");
 		this.driver = new FirefoxDriver(); 
 	}
 
---
+++ lkv2sele/src/main/java/helper/Constant.java
@@ -0,0 +1,7 @@
+package helper;
+
+public class Constant {
+	public static final String USERNAME 	= "test";
+	public static final String PASSWORD 	= "TEST";
+	public static final String REPOSITORY 	= "SampleRepository";
+}
