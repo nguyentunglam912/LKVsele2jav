c503bb35236db762c5141540de20be82748c2cb7
nguyentunglam912 <31235794+nguyentunglam912@users.noreply.github.com>
2020-03-18 17:24:44 +0700

    update framework (#4)
    Co-authored-by: Lam Nguyen <nguyentunglam912@gmail.com>

--- lkv2sele/src/test/java/testbase/TestBase.java
+++ lkv2sele/src/test/java/testbase/TestBase.java
@@ -1,39 +1,81 @@
 package testbase;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
+import org.apache.log4j.varia.NullAppender;
+import org.testng.ITestResult;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 
-import managers.DriverManager;
 import managers.DriverManagerFactory;
-import pageObjects.LoginPage;
 import helper.DriverUtils;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+
 public class TestBase {
-	public WebDriver driver;
-	DriverManager driverManager;
-	LoginPage LoginPage = new LoginPage();
+	static ExtentReports extent;
+	protected static ExtentTest test;
+	public static Logger logger = Logger.getLogger("Test");
 
 	@BeforeClass
-	public void setUp() {
+	public static void setUp() {
 		System.out.println("Pre-condition");
 		BasicConfigurator.configure();
 		DriverUtils.driver = DriverManagerFactory.getDriverManager(DriverUtils.browser).getWebDriver();
-		DriverUtils.driver.get(DriverUtils.url);
-		DriverUtils.driver.manage().window().maximize();
-		DriverUtils.driver.manage().timeouts().pageLoadTimeout(DriverUtils.loadTimeout,TimeUnit.SECONDS);
-		DriverUtils.driver.manage().timeouts().implicitlyWait(DriverUtils.implicityWait, TimeUnit.SECONDS);
 	}
 
 	@AfterClass
 	public void tearDown() {
 		System.out.println("Post-condition");
+		Logger.getRootLogger().removeAllAppenders();
+		Logger.getRootLogger().addAppender(new NullAppender());
 		DriverUtils.driver.quit();
 	}
 
-	public static final Logger log = Logger.getLogger(TestBase.class.getName());
+	@BeforeTest
+	public static void startReport() {
+		extent = new ExtentReports(System.getProperty("user.dir") +"/test-output/STMExtentReport.html", true);
+		extent.loadConfig(new File(System.getProperty("user.dir") + "extent-config.xml")); 
+		test = extent.startTest("");
+	}
+
+	@AfterMethod
+	public void getResult(ITestResult result){
+		if(result.getStatus() == ITestResult.FAILURE){
+			test.log(LogStatus.FAIL, "Test Case Failed is "+ result.getName());
+			test.log(LogStatus.FAIL, "Test Case Failed is "+ result.getThrowable());
+			} else if(result.getStatus() == ITestResult.SKIP){
+				test.log(LogStatus.SKIP, "Test Case Skipped is "+result.getName());
+				}
+		extent.endTest(test);
+		}
+
+	@AfterTest
+	public void endReport(){
+		extent.flush();
+		extent.close();
+    }
+
+	public void reportLog(String message) {
+		 test.log(LogStatus.INFO, message);
+		 logger.info("Message: " + message);
+		 Reporter.log(message);
+	}
+
+	public void goToDashboardLoginPage() {
+		DriverUtils.driver.get(DriverUtils.url);
+		DriverUtils.driver.manage().window().maximize();
+		DriverUtils.driver.manage().timeouts().pageLoadTimeout(DriverUtils.loadTimeout,TimeUnit.SECONDS);
+		DriverUtils.driver.manage().timeouts().implicitlyWait(DriverUtils.implicityWait, TimeUnit.SECONDS);
+	}
 }
