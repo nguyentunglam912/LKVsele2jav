9513226d9d3ea6ad4244044cd1f7ed3a7a5b4905
khangha2412 <61858234+khangha2412@users.noreply.github.com>
2020-03-25 21:34:44 +0700

    Completed TC4 6 7 Khang (#11)

--- src/main/java/com/sele2/support/Constant.java
+++ src/main/java/com/sele2/support/Constant.java
@@ -3,11 +3,14 @@ package com.sele2.support;
 public class Constant {
 	public static final String VALID_USERNAME 	= "test";
 	public static final String VALID_PASSWORD 	= "TEST";
+	public static final String UPPERCASE_USERNAME  = "TEST";
+	public static final String LOWERCASE_PASSWORD    = "test";
 	public static final String INVALID_USERNAME = "abc";
 	public static final String INVALID_PASSWORD = "abc";
 	public static final String BLANK_USERNAME 	= "";
 	public static final String BLANK_PASSWORD 	= "";
 	public static final String REPOSITORY 		= "SampleRepository";
+	public static final String REPOSITORY2      = "SampleRepositoryLV2";
 	public static final String CONFIG_PATH		= "\\src\\main\\java\\com\\sele2\\helper\\Configuration.properties";
 	public static final String ERROR_MESSAGE_INVALID_USERNAME_OR_PASSWORD = "Username or password is invalid";
 	public static final String ERROR_MESSAGE_PLEASE_ENTER_USERNAME = "Please enter username!";
---
+++ src/test/java/com/sele2/testcases/DA_LOGIN/DA_LOGIN_TC007.java
@@ -0,0 +1,47 @@
+package com.sele2.testcases.DA_LOGIN;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Listeners;
+
+import com.sele2.pages.HomePage;
+import com.sele2.pages.LoginPage;
+import com.sele2.support.Constant;
+import com.sele2.support.Log;
+import com.sele2.testcases.testbase.TestBase;
+
+import io.qameta.allure.*;
+import com.sele2.utils.listeners.TestListener;
+
+@Listeners({ TestListener.class })
+
+public class DA_LOGIN_TC007 extends TestBase {
+	/**
+	 * DA_LOGIN_TC007
+	 * @author khang.ha
+	 */
+
+	LoginPage loginPage = new LoginPage();
+	HomePage homePage = new HomePage();
+
+	@Test(description = "Verify that Username is not case sensitive")
+    @Description("Test Description: Verify that Username is not case sensitive")
+	public void DA_LOGIN_TC007_UsernameNotCaseSensitive() {
+		Log.info("Step 1: Navigate to Dashboard login page");
+		goToDashboardLoginPage();
+		
+		Log.info("Step 2: Login with the account has uppercase username");
+		loginPage.login(Constant.REPOSITORY, Constant.UPPERCASE_USERNAME, Constant.VALID_PASSWORD);
+
+		Log.info("VP: Verify that Dashboard Mainpage appears");
+		loginPage.checkLoginSuccessfully();
+		
+		Log.info("Step 3: Logout TA Dashboard");
+		homePage.logOut();
+
+		Log.info("Step 4: Login with the above account but enter lowercase username");
+		loginPage.login(Constant.REPOSITORY, Constant.VALID_USERNAME, Constant.VALID_PASSWORD);
+
+		Log.info("VP: Verify that Dashboard Mainpage appears");
+		loginPage.checkLoginSuccessfully(); 
+		}
+	}
