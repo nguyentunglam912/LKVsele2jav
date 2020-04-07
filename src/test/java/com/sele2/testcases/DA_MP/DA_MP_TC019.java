20211d5d594d7b979f3bc45bda98bb6944ab45d2
khang.ha <khang.ha@logigear.com>
2020-04-07 22:50:43 +0700

    done TC19

--- src/test/java/com/sele2/testcases/DA_MP/DA_MP_TC019.java
+++ src/test/java/com/sele2/testcases/DA_MP/DA_MP_TC019.java
@@ -1,6 +1,8 @@
 package com.sele2.testcases.DA_MP;
 
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
@@ -10,7 +12,6 @@ import com.sele2.testcases.testbase.TestBase;
 import com.sele2.utils.listeners.TestListener;
 
 import io.qameta.allure.Description;
-
 
 @Listeners({ TestListener.class })
 public class DA_MP_TC019 extends TestBase {
@@ -24,19 +25,25 @@ public class DA_MP_TC019 extends TestBase {
 	public void DA_MP_TC019_AddSiblingPageLevelToParentPage() {
 		Log.info("Step 1: Navigate to Dashboard login page");
 		goToDashboardLoginPage();
-		
+
 		Log.info("Step 2: Login with valid account");
 		loginPage.login(Constant.REPOSITORY, Constant.VALID_USERNAME, Constant.VALID_PASSWORD);
-		
+
 		Log.info("Step 3: Go to Global Setting -> Add page");
 		homePage.selectAddPageButtonInGlobalSettingMenu();
-		
+
 		Log.info("Step 4: Enter info into all required fields on New Page dialog");
 		newPage.submitNewPage(Constant.PAGE_NAME1, Constant.OVERVIEW_PAGE, null, null, null);
-		
+
 		Log.info("VP: User is able to add additional sibbling page levels to parent page successfully");
 		Assert.assertEquals(homePage.getCurrentPage(), Constant.PAGE_NAME1);
-				
+	}
+
+	@AfterMethod
+	private void cleanUp(ITestResult result) {
+		if (result.getStatus() == ITestResult.SUCCESS) {
+			homePage.deleteAllPagesFromMenu(Constant.OVERVIEW_PAGE);
+		}
 	}
 
 }
